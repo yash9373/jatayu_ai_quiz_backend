ace5023ee9ad5a25d70c5da3f16c30d92c03d060
Vedant Kotkar <vedantkotkar111@gmail.com>
2025-07-27 17:24:05 +0530

    Fixed alembic migration

--- alembic/versions/20250726_add_priority_node_counts_to_test.py
+++ alembic/versions/20250726_add_priority_node_counts_to_test.py
@@ -6,14 +6,19 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'a1b2c3d4e5f6'
-down_revision = 'e84d26a977f1'
+down_revision = '7c05a3cbc49b'
 branch_labels = None
 depends_on = None
 
+
 def upgrade():
-    op.add_column('tests', sa.Column('high_priority_nodes', sa.Integer(), nullable=True, server_default='0'))
-    op.add_column('tests', sa.Column('medium_priority_nodes', sa.Integer(), nullable=True, server_default='0'))
-    op.add_column('tests', sa.Column('low_priority_nodes', sa.Integer(), nullable=True, server_default='0'))
+    op.add_column('tests', sa.Column('high_priority_nodes',
+                  sa.Integer(), nullable=True, server_default='0'))
+    op.add_column('tests', sa.Column('medium_priority_nodes',
+                  sa.Integer(), nullable=True, server_default='0'))
+    op.add_column('tests', sa.Column('low_priority_nodes',
+                  sa.Integer(), nullable=True, server_default='0'))
+
 
 def downgrade():
     op.drop_column('tests', 'high_priority_nodes')
--- alembic/versions/a0c5d9f03b3a_merge_heads.py
+++ alembic/versions/a0c5d9f03b3a_merge_heads.py
@@ -1,7 +1,7 @@
 """Merge heads
 
 Revision ID: a0c5d9f03b3a
-Revises: a1b2c3d4e5f6, e84d26a977f1
+Revises: a1b2c3d4e5f6
 Create Date: 2025-07-26 22:31:22.874165
 
 """
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = 'a0c5d9f03b3a'
-down_revision: Union[str, Sequence[str], None] = ('a1b2c3d4e5f6', 'e84d26a977f1')
+down_revision: Union[str, Sequence[str], None] = 'a1b2c3d4e5f6'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
